ac1d269f9322c9ae1213824a6132e5f20951004d
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-07-02 15:57:44 +0300

    full stack dev react and flask

--- server/app.py
+++ server/app.py
@@ -16,11 +16,33 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    if request.method == 'GET':
+        return jsonify(message.to_dict())
+
+    if request.method == 'PATCH':
+        data = request.json
+        if 'body' in data:
+            message.body = data['body']
+        db.session.commit()
+        return jsonify(message.to_dict())
+
+    if request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return '', 204
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.json
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
 
 if __name__ == '__main__':
     app.run(port=5555)
